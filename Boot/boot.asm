b60287ac38c97da2ebc21178d1df786994e0bab0
liyu <liyu26@meituan.com>
2017-12-15 12:21:20 +0800

    新增多次调用

--- Boot/boot.asm
+++ Boot/boot.asm
@@ -1,22 +1,32 @@
 ;跳转到 7c00 处,此处开始是boot程序的机器码
 org 0x7c00
 
-call disp
+call hello
 htl
-;jmp $
-disp:  
+
+hello:
 	mov ax, message
 	mov bp, ax
 	mov ah, 13h     ;写字符串
 	mov al, 1h      ;写模式
-	;mov ax, 0x1301
-	mov bx, 0xa     ;字符颜色
-	mov cx, 0x14    ;字符长度
-	mov dx, 0x0203  
+	mov bx, 0x9     ;字符颜色
+	mov cx, 0xb     ;字符长度
+	mov dx, 0x0202  ;行 列
+	int 10h         ;调用biso的函数
+
+	mov ax, info
+	mov bp, ax
+	mov ah, 13h     ;写字符串
+	mov al, 1h      ;写模式
+	mov bx, 0xd     ;字符颜色
+	mov cx, 0x7     ;字符长度
+	mov dx, 0x0302  ;行 列
 	int 10h         ;调用biso的函数
 	ret
 message:
-	db "Hello World! HeleeOS"
+	db "Hello World!"
+info:
+	db "HeleeOS"
 
 ;后续填充0
 times 446-($-$$) db 0
